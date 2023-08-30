ee0013574f3d2a009dbef3fbc6a6fb8fb8460791
Patryk Jaroszek <jerrymotovlog@gmail.com>
2023-08-30 16:31:03 +0200

    Change botton navigation bar index  to 0 on save

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 
 class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
+    required this.onSave,
     super.key,
   });
+
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -18,52 +21,58 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Podaj nazwę restauracji',
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Podaj nazwę restauracji',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
             ),
-            onChanged: (newValue) {
-              setState(() {
-                restaurantName = newValue;
-              });
-            },
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Podaj nazwę kebaba',
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Podaj nazwę kebaba',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  kebabName = newValue;
+                });
+              },
             ),
-            onChanged: (newValue) {
-              setState(() {
-                kebabName = newValue;
-              });
-            },
-          ),
-          Slider(
-            onChanged: (newValue) {
-              setState(() {
-                reating = newValue;
-              });
-            },
-            value: reating,
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: reating.toString(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore.instance.collection('restaurants').add({
-                'name': restaurantName,
-                'kebab': kebabName,
-                'reating': reating,
-              });
-            },
-            child: const Text('Dodaj'),
-          ),
-        ],
+            Slider(
+              onChanged: (newValue) {
+                setState(() {
+                  reating = newValue;
+                });
+              },
+              value: reating,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: reating.toString(),
+            ),
+            ElevatedButton(
+              onPressed: restaurantName.isEmpty || kebabName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('restaurants').add({
+                        'name': restaurantName,
+                        'kebab': kebabName,
+                        'reating': reating,
+                      });
+                      widget.onSave();
+                    },
+              child: const Text('Dodaj'),
+            ),
+          ],
+        ),
       ),
     );
   }
