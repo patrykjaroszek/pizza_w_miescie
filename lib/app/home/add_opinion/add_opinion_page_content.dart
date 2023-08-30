335f25695af37b7f75242af7e52b2ad1136310ff
Patryk Jaroszek <jerrymotovlog@gmail.com>
2023-08-30 15:45:58 +0200

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     super.key,
   });
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var kebabName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę restauracji',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę kebaba',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                kebabName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'kebab': kebabName,
+                'reating': 3.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
