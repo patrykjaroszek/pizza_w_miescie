e43ea2093fba0ef61f9b364c52ad586d11216179
Patryk Jaroszek <jerrymotovlog@gmail.com>
2023-10-12 15:24:46 +0200

    Move home and login into features folder

--- lib/app/features/home/restaurants/restaurants_page_contetnt.dart
+++ lib/app/features/home/restaurants/restaurants_page_contetnt.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pizza_w_miescie/app/home/restaurants/cubit/restaurants_cubit.dart';
+import 'package:pizza_w_miescie/app/features/home/restaurants/cubit/restaurants_cubit.dart';
 
 class RestaurantsPageContent extends StatelessWidget {
   const RestaurantsPageContent({
